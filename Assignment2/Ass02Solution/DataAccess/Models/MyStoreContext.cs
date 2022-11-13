134ea93b63ce872b81ddfd5c712d91c106965d7a
anhtnse <trinhnamanh0909@gmail.com>
2022-11-13 23:03:18 +0700

    Create Models

---
+++ Assignment2/Ass02Solution/DataAccess/Models/MyStoreContext.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Models;
+
+public partial class MyStoreContext : DbContext
+{
+    public MyStoreContext()
+    {
+    }
+
+    public MyStoreContext(DbContextOptions<MyStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Member> Members { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(Helper.JsonReader.GetConnectionString());
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Member>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__Member__0CF04B187CE5B46C");
+
+            entity.ToTable("Member");
+
+            entity.HasIndex(e => e.Email, "UQ__Member__A9D10534939F814E").IsUnique();
+
+            entity.Property(e => e.City)
+                .IsRequired()
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.CompanyName)
+                .IsRequired()
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .IsRequired()
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Order__C3905BCF435475A3");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.Freight).HasColumnType("money");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.RequiredDate).HasColumnType("datetime");
+            entity.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Member).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.MemberId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Order__MemberId__2A4B4B5E");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.ProductId, e.OrderId }).HasName("PK__OrderDet__5835C37136FE9112");
+
+            entity.ToTable("OrderDetail");
+
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__OrderDeta__Order__300424B4");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__OrderDeta__Produ__30F848ED");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Product__B40CC6CD8783F75F");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ProductName)
+                .IsRequired()
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+            entity.Property(e => e.Weight)
+                .IsRequired()
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
