a768584f0d1483c12653c63cd62c00e7a1f767a1
quandoivietnam <trinhnamanh0909@gmail.com>
2022-11-15 09:31:01 +0700

    Config

---
+++ Assignment2/Ass02Solution/SalesWinApp/frmAddDetail.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SalesWinApp
+{
+    partial class frmAddDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbUnitPrice = new System.Windows.Forms.Label();
+            this.lbDiscount = new System.Windows.Forms.Label();
+            this.lbQuantity = new System.Windows.Forms.Label();
+            this.lbProductId = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cboProductId = new System.Windows.Forms.ComboBox();
+            this.txtDiscount = new System.Windows.Forms.MaskedTextBox();
+            this.txtQuantity = new System.Windows.Forms.MaskedTextBox();
+            this.txtUnitPrice = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // lbUnitPrice
+            // 
+            this.lbUnitPrice.AutoSize = true;
+            this.lbUnitPrice.Location = new System.Drawing.Point(58, 96);
+            this.lbUnitPrice.Name = "lbUnitPrice";
+            this.lbUnitPrice.Size = new System.Drawing.Size(72, 20);
+            this.lbUnitPrice.TabIndex = 29;
+            this.lbUnitPrice.Text = "Unit Price";
+            // 
+            // lbDiscount
+            // 
+            this.lbDiscount.AutoSize = true;
+            this.lbDiscount.Location = new System.Drawing.Point(57, 190);
+            this.lbDiscount.Name = "lbDiscount";
+            this.lbDiscount.Size = new System.Drawing.Size(67, 20);
+            this.lbDiscount.TabIndex = 27;
+            this.lbDiscount.Text = "Discount";
+            // 
+            // lbQuantity
+            // 
+            this.lbQuantity.AutoSize = true;
+            this.lbQuantity.Location = new System.Drawing.Point(57, 143);
+            this.lbQuantity.Name = "lbQuantity";
+            this.lbQuantity.Size = new System.Drawing.Size(65, 20);
+            this.lbQuantity.TabIndex = 25;
+            this.lbQuantity.Text = "Quantity";
+            // 
+            // lbProductId
+            // 
+            this.lbProductId.AutoSize = true;
+            this.lbProductId.Location = new System.Drawing.Point(57, 48);
+            this.lbProductId.Name = "lbProductId";
+            this.lbProductId.Size = new System.Drawing.Size(73, 20);
+            this.lbProductId.TabIndex = 23;
+            this.lbProductId.Text = "ProductId";
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSave.Location = new System.Drawing.Point(263, 249);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 31;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(382, 249);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 29);
+            this.btnCancel.TabIndex = 32;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // cboProductId
+            // 
+            this.cboProductId.FormattingEnabled = true;
+            this.cboProductId.Location = new System.Drawing.Point(190, 45);
+            this.cboProductId.Name = "cboProductId";
+            this.cboProductId.Size = new System.Drawing.Size(255, 28);
+            this.cboProductId.TabIndex = 33;
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(190, 187);
+            this.txtDiscount.Mask = "000.00";
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(255, 27);
+            this.txtDiscount.TabIndex = 34;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(190, 140);
+            this.txtQuantity.Mask = "00000";
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(255, 27);
+            this.txtQuantity.TabIndex = 35;
+            this.txtQuantity.ValidatingType = typeof(int);
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(190, 93);
+            this.txtUnitPrice.Mask = "00000000";
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(255, 27);
+            this.txtUnitPrice.TabIndex = 36;
+            this.txtUnitPrice.ValidatingType = typeof(int);
+            // 
+            // frmAddDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 312);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.cboProductId);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lbUnitPrice);
+            this.Controls.Add(this.lbDiscount);
+            this.Controls.Add(this.lbQuantity);
+            this.Controls.Add(this.lbProductId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmAddDetail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmAddDetail";
+            this.Load += new System.EventHandler(this.frmAddDetail_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbUnitPrice;
+        private System.Windows.Forms.Label lbDiscount;
+        private System.Windows.Forms.Label lbQuantity;
+        private System.Windows.Forms.Label lbProductId;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cboProductId;
+        private System.Windows.Forms.MaskedTextBox txtDiscount;
+        private System.Windows.Forms.MaskedTextBox txtQuantity;
+        private System.Windows.Forms.MaskedTextBox txtUnitPrice;
+    }
+}
---
+++ Assignment2/Ass02Solution/SalesWinApp/frmAddDetail.cs
@@ -0,0 +1,94 @@
+﻿using DataAccess.Models;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesWinApp
+{
+    public partial class frmAddDetail : Form
+    {
+        IProductRepository productRepository = new ProductRepository();
+        IOrderDetailsRepository orderDetailsRepository = new OrderDetailsRepository();
+        public Order Order { get; set; }
+        public OrderDetail Detail { get; set; }
+        public bool InsertOrUpdate { get; set; } = true;
+        public frmAddDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAddDetail_Load(object sender, EventArgs e)
+        {
+            var listProduct = productRepository.Get();
+            if (listProduct != null && listProduct.Count() > 0)
+            {
+                cboProductId.DataSource = listProduct;
+                cboProductId.DisplayMember = "ProductName";
+                cboProductId.SelectedValue = "ProductId";
+            }
+            if (!InsertOrUpdate)
+            {
+                if (Detail != null)
+                {
+                    txtUnitPrice.Text = Detail.UnitPrice.ToString();
+                    txtQuantity.Text = Detail.Quantity.ToString();
+                    txtDiscount.Text = Detail.Discount.ToString();
+                    cboProductId.SelectedValue = Detail.ProductId.ToString();
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var detail = new OrderDetail
+            {
+                OrderId = Order.OrderId,
+                ProductId = (int)cboProductId.SelectedValue,
+                UnitPrice = decimal.Parse(txtUnitPrice.Text),
+                Quantity = int.Parse(txtQuantity.Text),
+                Discount = double.Parse(txtDiscount.Text),
+            };
+            if (InsertOrUpdate)
+            {
+                bool update = GetUpdateProduct(detail.ProductId, detail.Quantity);
+                if (update)
+                {
+                    orderDetailsRepository.Add(detail);
+                }
+            }
+            else
+            {
+                bool update = GetUpdateProduct(detail.ProductId, detail.Quantity);
+                if (update)
+                {
+                    orderDetailsRepository.Update(detail);
+                }
+            }
+        }
+        private bool GetUpdateProduct(int productId, int quantity)
+        {
+            bool rs = false;
+            var product = productRepository.Get().Where(p => p.ProductId == productId).FirstOrDefault();
+            if (product != null)
+            {
+                if (product.UnitPrice > 0 && product.UnitPrice > quantity)
+                {
+                    product.UnitInStock -= quantity;
+                    rs = true;
+                }
+                else
+                {
+                    MessageBox.Show("Not enough quantity");
+                }
+            }
+            return rs;
+        }
+    }
+}
